c981f5188579071721235ec020f6709a75f843b1
Inderpreet02 <inder192002@gmail.com>
2021-01-07 00:17:51 +0530

    day 2 almost done but have to fix transition issue with clock hands

---
+++ Day 2/script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".sec-hand")
+const minuteHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 60) * 360) + 90;
+    console.log(seconds);
+    secondHand.style.transform = `rotateZ(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotateZ(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotateZ(${hoursDegrees}deg)`;
+    // console.log("hi");
+}
+
+setInterval(setDate, 1000);
